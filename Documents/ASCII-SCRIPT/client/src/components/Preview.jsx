13f44dc1016e244eca4a92a2f737b893979be883
lgardner <lgardner@sentryviewsystems.com>
2021-11-29 19:49:31 -0500

    initial commit

---
+++ Documents/ASCII-SCRIPT/client/src/components/Preview.jsx
@@ -0,0 +1,11 @@
+import { AppBar, Button, Toolbar, Typography, Container } from "@mui/material"
+import { makeStyles } from "@mui/styles";
+import
+
+const Preview = () => {
+    return 
+        <h1>uadvboh</h1>
+    
+};
+
+export default Preview;
